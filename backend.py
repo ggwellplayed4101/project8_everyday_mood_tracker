fe2cbaa83a08128c937ac7357ee9bd249c8c183f
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-20 13:02:18 -0500

    Extract positivity index and negative index from diary entry

--- backend.py
+++ backend.py
@@ -2,15 +2,24 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import glob
 import os
 
-files = glob.glob("diary/*.txt") 
-dates = [os.path.splitext(os.path.basename(file))[0] for file in files]
-scores = []
 
-print(dates)
+def read_mood_of(folder_path="diary/*.txt"):
 
-for file in files:
-    print(file)
-    with open(file, "r"):
-        analyzer = SentimentIntensityAnalyzer()
+    files = glob.glob(folder_path) 
+    dates = [os.path.splitext(os.path.basename(file))[0] for file in files]
+    sentiments = []
 
+    for file_path in files:
+        with open(file_path, "r") as file:
+            text = file.read()
+            analyzer = SentimentIntensityAnalyzer()
+            sentiment = analyzer.polarity_scores(text)
+            sentiments.append(sentiment)
 
+    positivity_indexes = [item["pos"] for item in sentiments]
+    negativity_indexes = [item["neg"] for item in sentiments]
+
+    return (positivity_indexes, negativity_indexes, dates)
+
+if __name__=="__main__":
+    read_mood_of()
